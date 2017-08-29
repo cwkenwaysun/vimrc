ad41680724111c1b14d5b71c5665c37e7b4fa841
孫健瑋 <zyphire@sonkens-mbp.uconnect.utah.edu>
2017-08-29 14:41:52 -0600

    add Vundle installation

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -2,3 +2,6 @@
 
 cat basic.vim > ~/.vimrc
 echo "Installed successfully! Reminder: Please check if you have install vundle."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installed Vundle."
+echo "You are all set!"
